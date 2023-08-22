c85a9ce05cd003cdf92695663a61975353251c06
Coder-Edge <141169535+Coder-Edge@users.noreply.github.com>
2023-08-22 02:05:17 +0200

    Update Decrypt_cesar.py

--- Decrypt_cesar.py
+++ Decrypt_cesar.py
@@ -1,29 +1,29 @@
+#Decrypt without key
 
-# list_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
-#     , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#
-# for x in range(len(list_letter)):
-#     list_letter.append(list_letter[x])
-#
-# message = input("Entrez votre message : ")
-# clef = int(input("Entrez votre cle : "))
-#
-# def encrypte_cesar(lettre,liste,clef):
-#     for i in range(len(liste)):
-#         if lettre == ' ':
-#             return ' '
-#         elif liste[i] == lettre:
-#             return str(list_letter[i+clef])
-#
-#     return '?'
-#
-#
-#
-# for clef in range(1,26):
-#     encrypte_message = str()
-#     for letter in message:
-#         encrypte_message += encrypte_cesar(letter,list_letter,clef)
-#     print(encrypte_message)
+list_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
+    , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for x in range(len(list_letter)):
+    list_letter.append(list_letter[x])
+
+message = input("Entrez votre message : ")
+clef = int(input("Entrez votre cle : "))
+
+def encrypte_cesar(lettre,liste,clef):
+    for i in range(len(liste)):
+        if lettre == ' ':
+            return ' '
+        elif liste[i] == lettre:
+            return str(list_letter[i+clef])
+
+    return '?'
+
+
+for clef in range(1,26):
+    encrypte_message = str()
+    for letter in message:
+        encrypte_message += encrypte_cesar(letter,list_letter,clef)
+    print(encrypte_message)
 
 # Descrypte with a key
 
